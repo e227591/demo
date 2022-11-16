d52f302ab979108d37d72e9221d819c0b1394a45
e227591 <e227591@sbl.ch>
2022-11-16 14:45:07 +0100

    change to red

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ function handleKeyPress (ev) {
         player.setAttribute ("cx", 100 * Math.random ())
         player.setAttribute ("cy", 100 * Math.random ())
 
+    } else if (ev.key === "r") {
+        player.setAttribute ("fill", "red")
     }
 
 }
