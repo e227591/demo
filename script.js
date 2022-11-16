a6a890424f335a0be1844e05247c90100db03d0d
e227591 <e227591@sbl.ch>
2022-11-16 14:41:57 +0100

    Only jump on space

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function handleKeyPress (ev) {
-    const player = document.querySelector ("#player")
-    player.setAttribute ("cx", 100 * Math.random ())
-    player.setAttribute ("cy", 100 * Math.random ())
+    if (ev.code === "Space") {
+        const player = document.querySelector ("#player")
+        player.setAttribute ("cx", 100 * Math.random ())
+        player.setAttribute ("cy", 100 * Math.random ())
+
+    }
+
 }
 
 
